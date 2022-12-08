d3c476adf15dc6aa76209a5b5438243a5c4d2d99
setyawan-andy <andy.asbi@gmail.com>
2022-12-09 06:39:15 +0700

    adding topbar init component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { ColorModeContext, useMode } from "../theme"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+
+import Topbar from "../views/global/Topbar"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [theme, colorMode] = useMode()
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+            <Component {...pageProps} />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
 }
---
+++ views/global/Topbar.tsx
@@ -0,0 +1,18 @@
+import { Box, IconButton, useTheme } from "@mui/material"
+import { useContext } from "react"
+import { ColorModeContext, tokens } from "../../theme"
+import InputBase from "@mui/material"
+
+// icons
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined"
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined"
+import SearchIcon from "@mui/icons-material/Search"
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined"
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined"
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined"
+
+const Topbar = () => {
+  return <div>Topbar</div>
+}
+
+export default Topbar
